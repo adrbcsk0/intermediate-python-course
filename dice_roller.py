3f1f8efd5b3c86bed41642682c80b01c286bc383
adrbcsk0 <78171744+adrbcsk0@users.noreply.github.com>
2021-02-04 10:59:38 +0100

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -5,8 +5,13 @@ def main():
   dice_sum=0
   for i in range (0, dice_rolls):
     roll=random.randint(1,6)
-    dice_sum+=roll 
-    print(f'You rolled a {roll}')
+    dice_sum+=roll
+    if roll==1:
+      print(f'Siiiiick!{roll}')
+    elif roll==6:
+      print(f'Crazy mudda!{roll}')      
+    else:
+      print(f'You rolled a {roll}')
   print(f'Your total:{dice_sum}')
   
   
